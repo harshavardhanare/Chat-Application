88845da68aaeef04175685c44954a0a295add59e
harshavardhanare <harshachowdary35@gmail.com>
2024-10-19 08:15:47 +0530

    first commit

---
+++ backend/config/db.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect("mongodb://localhost:27017", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(`Error Found: ${error.message}`);
+    process.exit();
+  }
+};
+module.exports = connectDB;
